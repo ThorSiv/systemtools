c33105b274431b3de56942ee38df3a12af16823c
thorsiv <thorsivodin@gmail.com>
2021-09-29 01:19:25 +0800

    initsystem

--- initsystem.sh
+++ initsystem.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 set -e
+echo 
+echo 
+echo 
 echo "##############################################################"
 echo " This script is to clear system  and install softwares needed."
 echo " You must know what is going on before running this script."
 echo "               Good luck for your life !"
 echo "##############################################################"
-echo "done"
+echo 
+echo
+echo
+
+
+echo "Disable Selinux..............."
+
+if [ `getenforce` != "Disabled" ];then
+	setenforce 0
+	sed -i "s/^SELINUX=/SELINUX=Disabled/g" /etc/selinux/config
+fi
+
+
+echo "Install softwares needed......."
+
+yum -y install git nmap iftop $>/dev/null
+
