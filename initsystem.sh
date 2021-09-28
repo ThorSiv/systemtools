142d29df0dbf951f168a26a3cebef67d1e294b47
thorsiv <thorsivodin@gmail.com>
2021-09-29 00:36:36 +0800

    add initsystem.sh

---
+++ initsystem.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+echo "##############################################################"
+echo " This script is to clear system  and install softwares needed."
+echo " You must know what is going on before running this script."
+echo "               Good luck for your life !"
+echo "##############################################################"
+echo "done"
